45eba513af2a2f58498a6c1e2afcd1efac04e7f8
jacobromero <jacob22romero@gmail.com>
2016-10-11 09:24:55 -0700

    Initial commit

---
+++ iVoteSimulator/src/IVoteService.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Jacob Romero on 10/11/2016.
+ */
+public class IVoteService {
+}
---
+++ iVoteSimulator/src/SimulationDriver.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Jacob Romero on 10/11/2016.
+ */
+public class SimulationDriver {
+    public static void main(String[] args) {
+    }
+}
